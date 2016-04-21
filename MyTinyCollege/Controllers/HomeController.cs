d20ad67ba415ef0ac8851f0ff7ccd40ad1de7841
Greg <Greg@AVDP-GLAURETTE>
2016-04-21 13:20:11 -0300

    Add project files.

---
+++ MyTinyCollege/Controllers/HomeController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MyTinyCollege.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+
+        public ActionResult Test()
+        {
+            //return Content("<h1>This is just a test</h1>");
+            return View();
+        }
+    }
+}
